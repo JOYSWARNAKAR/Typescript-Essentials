538c097115e223e6d3d84dcccf1e73bb39f2677c
[Joy Swarnakar] <[joyswarnakar3@gmail.com]>
2026-02-24 23:19:36 +0530

    changes folders and files names

--- part2/src/index.ts
+++
@@ -1,8 +0,0 @@
-function greet(person:string) : string{
-    return `Hello ${person}, Welcome to RealCoding1.`
-}
-console.log(greet("Joy"));
-
-const username: string = "realDev"
-console.log(greet(username));
-
